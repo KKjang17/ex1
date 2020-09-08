24f1d350acad783670de8b9b828cdf707f90e322
Kay <kaydenkj17@gmail.com>
2020-09-07 23:43:02 -0400

    Exercise 1.4 completed

--- src/Total.js
+++ src/Total.js
@@ -4,7 +4,7 @@ const Total = (props) => {
     console.log(props)
     return (
         <div>
-            <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
+            <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
         </div>
     )
 }
